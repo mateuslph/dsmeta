50a9c60acfa8c4a1c0fdef4f6887eb4927f4d7bc
mateuslph <mlptche@gmail.com>
2022-07-11 21:13:18 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import icon from '../../assetes/img/notification-icon.svg'
-import './styles.css'
+import icon from '../../assetes/img/notification-icon.svg';
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -9,4 +9,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
